b9b9386fd1fa5cf25e06a934c58c856a55804753
mostafa-fathi <mostafafathy677@gmail.com>
2019-12-23 15:13:20 +0200

    update

--- app/src/main/java/com/example/test/ChatPage.java
+++ app/src/main/java/com/example/test/ChatPage.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
@@ -22,7 +23,16 @@ public class ChatPage extends AppCompatActivity{
     TextView textView;
     EditText message;
     ImageButton send;
-    String[] listItem= {"1.0", "1.1", "1.5", "1.6", "2.0", "2.2", "2.3", "3.0", "4.0", "4.1", "4.4", "5.0", "6.0", "7.0"};
+    String[] version = {"Android Alpha", "Android Beta", "Android Cupcake", "Android Donut", "Android Eclair", "Android Froyo", "Android Gingerbread", "Android Honeycomb", "Android Ice Cream Sandwich", "Android JellyBean", "Android Kitkat", "Android Lollipop", "Android Marshmallow", "Android Nougat"};
+
+    String[] versionNumber = {"1.0", "1.1", "1.5", "1.6", "2.0", "2.2", "2.3", "3.0", "4.0", "4.1", "4.4", "5.0", "6.0", "7.0"};
+    int[] images = {R.mipmap.ic_launcher, R.mipmap.ic_launcher,R.mipmap.ic_launcher, R.mipmap.ic_launcher,R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher,R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher,R.mipmap.ic_launcher};
+
+
+    ListView lView;
+
+
+    ChatPageListAdaptor lAdapter;
     ArrayList<String> stringArrayList=new ArrayList<>();
     ArrayAdapter adapter;
     @Override
@@ -30,40 +40,44 @@ public class ChatPage extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_page);
         listView=(ListView)findViewById(R.id.chatpage_listview);
-        textView=(TextView)findViewById(R.id.mylisttexxtview);
+
         message=(EditText)findViewById(R.id.chatpage_message_et);
         send=(ImageButton)findViewById(R.id.chatpage_send_imageButton);
         message.setScroller(new Scroller(this));
         message.setMaxLines(5);
         message.setVerticalScrollBarEnabled(true);
         message.setMovementMethod(new ScrollingMovementMethod());
-        stringArrayList.addAll(Arrays.asList(listItem));
+        stringArrayList.addAll(Arrays.asList(version));
+        lView = (ListView) findViewById(R.id.chatmenu_listview);
+
+        lAdapter = new ChatPageListAdaptor(this, version, versionNumber, images);
+
+        lView.setAdapter(lAdapter);
+
+        lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(ChatPage.this, version[i]+" "+versionNumber[i], Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String sms=message.getText().toString();
                 stringArrayList.add(sms);
-                adapter = new ArrayAdapter<String>(ChatPage.this,
-                        android.R.layout.simple_list_item_1, android.R.id.text1, stringArrayList);
-                listView.setAdapter(adapter);
+                lAdapter = new ChatPageListAdaptor(ChatPage.this, version, versionNumber, images);
+
+                lView.setAdapter(lAdapter);
 
             }
         });
 
 
-        adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, stringArrayList);
-        listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // TODO Auto-generated method stub
-                String value=adapter.getItem(position).toString();
-                Toast.makeText(ChatPage.this,value,Toast.LENGTH_SHORT).show();
-
-            }
-        });
 
 
     }
